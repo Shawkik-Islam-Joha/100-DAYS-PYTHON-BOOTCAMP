1d36cb798aa86225b8d55066cb66dc8193a85123
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-08-09 17:29:55 +0600

    fix: correct variable naming for consistency in Hangman game

--- 100_Days_of_Python/Day_7_Hangman/project_hangman.py
+++ 100_Days_of_Python/Day_7_Hangman/project_hangman.py
@@ -9,8 +9,8 @@ x = int(input("Enter 1 to start, Enter 0 to end.\n"))
 if x == 0:
     exit()
 
-The_Mystery_Word = random.choice(words_for_hangman.words)
-length = len(The_Mystery_Word)
+the_mystery_word = random.choice(words_for_hangman.words)
+length = len(the_mystery_word)
 life = 6
 your_guess = list("_"*length)
 
@@ -18,7 +18,7 @@ print("".join(your_guess))
 
 while ("_" in your_guess) and life > 0:
     ch = input("Enter Your Guess: ")
-    idx = The_Mystery_Word.find(ch)
+    idx = the_mystery_word.find(ch)
     if idx != -1:
         your_guess[idx] = ch
     else:
@@ -26,7 +26,7 @@ while ("_" in your_guess) and life > 0:
         print(f"Wrong Guess! Chance left : {life}")
     print("".join(your_guess))              # Convert the list to string to print
 
-if "".join(your_guess) == The_Mystery_Word:
+if "".join(your_guess) == the_mystery_word:
     print("Congratulations!\nYou win!")
 elif life == 0:
     print("You Lose!")
