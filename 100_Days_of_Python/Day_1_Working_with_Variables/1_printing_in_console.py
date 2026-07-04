0e26fa1543c3eb2d7940d5586168d24a46bcffd7
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2026-07-04 13:49:12 +0600

    feat: add greeting message to the console output

--- 100_Days_of_Python/Day_1_Working_with_Variables/1_printing_in_console.py
+++ 100_Days_of_Python/Day_1_Working_with_Variables/1_printing_in_console.py
@@ -3,5 +3,6 @@ print("2. Knead the dough for 10 minutes.")
 print("3. Add 3g of Salt.")
 print("4. Leave to rise for 2 hours.")
 print("5. Bake at 200 degrees C for 30 minutes.")
+print("Hi, Joha!")
 
 # Prints the strings in console
